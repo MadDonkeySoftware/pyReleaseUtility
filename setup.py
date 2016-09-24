d62cabc1511ae313324bbb2a1a92fd83278dfb67
fritogotlayed <fritogotlayed@gmail.com>
2016-09-24 14:54:31 -0500

    First cut at building a deployment package

--- setup.py
+++ setup.py
@@ -9,9 +9,18 @@ dashboard_req = [str(ir.req) for ir in dashboard_req_contents]
 
 
 setup(
+    # App Name
     name='pyReleaseUtil',
+
+    # Version Number
     version='0.0.1',
-    packages=['web_dashboard'],
+
+    # Application Author
+    author="MadDonkey Software",
+
+    packages=['web_dashboard',
+              'web_dashboard.web',
+              'web_dashboard.web.siteroot'],
     install_requires=dashboard_req,
     entry_points={
         'web_dashboard': [
